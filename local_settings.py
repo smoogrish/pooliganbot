916a5c351ece0c059b999ea20c2df2d966371ddc
smoogrish <smoogrish@gmail.com>
2019-11-13 11:03:59 -0500

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -20,7 +20,7 @@ CLIENT_CRED_FILENAME = '' # the MASTODON client secret file you created for this
 USER_ACCESS_FILENAME = '' # The MASTODON user credential file you created at installation.
 
 # Sources (Twitter, Mastodon, local text file or a web page)
-TWITTER_SOURCE_ACCOUNTS = ["d_twit","chelsea_tuttle"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+TWITTER_SOURCE_ACCOUNTS = ["d_twit","chelsea_tuttle","iamkid_a","denisetut","jojofromjerz"]  # A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 MASTODON_SOURCE_ACCOUNTS = [""] # A list, e.g. ["@user@instance.tld"]
 SOURCE_EXCLUDE = r'^$'  # Source tweets that match this regexp will not be added to the Markov chain. You might want to filter out inappropriate words for example.
 STATIC_TEST = False  # Set this to True if you want to test Markov generation from a static file instead of the API.
@@ -34,8 +34,8 @@ ODDS = 1  # How often do you want this to run? 1/8 times?
 ORDER = 2  # How closely do you want this to hew to sensical? 2 is low and 4 is high.
 
 DEBUG = False  # Set this to False to start Tweeting live
-TWEET_ACCOUNT = "pooliganb"  # The name of the account you're tweeting to.
+TWEET_ACCOUNT = "drunkpoolboy"  # The name of the account you're tweeting to.
 
 #Configuration for Twitter parser. TEST_SOURCE will be re-used as as the corpus location.
 TWITTER_ARCHIVE_NAME = "tweets.csv" #Name of your twitter archive
-IGNORE_RETWEETS = True #If you want to remove retweets
+IGNORE_RETWEETS = False #If you want to remove retweets
